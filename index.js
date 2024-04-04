379d1b25645fd9f0503092b7f32537d760f57fb6
bobbyhadz <opmftw@gmail.com>
2024-04-04 08:39:14 +0300

    minor changes

---
+++ index.js
@@ -0,0 +1,9 @@
+// onclick not working in JavaScript
+
+console.log('bobbyhadz.com');
+
+const button = document.getElementById('btn');
+
+button.addEventListener('click', event => {
+  console.log('button clicked');
+});
